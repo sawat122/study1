00e72828586eb47f927ed6d4406c98d1dc8435bf
Zehra Aykan <120047575+sawat122@users.noreply.github.com>
2022-12-07 20:53:09 +0300

    Update study1.py

--- study1.py
+++ study1.py
@@ -1,23 +1,37 @@
+# Kullanıcı Giriş örneği
 
-"""Kullanıcı girişi yapmak"""
+print("""
+ *******************************
+    Kullanıcı Girişi Programı
+ *******************************
+    """)
+
+username = "zehra"
+
+parola = "1234"
+
+hak = 3
+nameDogru = False
 
 
+for i in range(hak):
+    if not nameDogru:
+        name = input("Kullanici adınızı giriniz: ")
+    pw = input("Parolanızı giriniz: ")
+    if name == username:
+        nameDogru = True
+        if pw == parola:
+            print("Giriş Başarılı!!")
+            break
 
-
-print("""****************************
-    Kullanıcı Girişi
-****************************""")
-
-username ="zehra"
-password = "1234"
-
-user = input("Kullanıcı adınızı giriniz:")
-
-if user == username:
-    psw = input("Parolanızı giriniz: ")
-    if psw ==password:
-        print("Giriş Başarılı Hoşgeldiniz!!")
+        else:
+            hak -= 1
+            print("Parola Hatalı!!Kalan hakkınız:{}".format(hak))
+            continue
     else:
-        print("Giriş Başarısız Tekrar Deneyiniz!")
-else:
-    print("Giriş Başarısız Tekrar Deneyiniz!") 
+        hak -= 1
+        print("Kullanıcı adı hatalı!! Kalan hakkınız: {}".format(hak))
+        
+    if hak == 0:
+        print("Giriş Hakkınız Bitti...")
+        break
